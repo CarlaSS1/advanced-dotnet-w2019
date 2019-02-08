584ad0ea4526dd96104061dbbe57c764947cd19f
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2019-02-07 20:01:25 -0500

    fixed the execution of methods using attributes to take into account methods with parameters

--- Week5Attributes/MyCustomAttribute.cs
+++ Week5Attributes/MyCustomAttribute.cs
@@ -1,24 +1,23 @@
 ﻿/*
  * Copyright 2016-2019 Mohawk College of Applied Arts and Technology
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you 
- * may not use this file except in compliance with the License. You may 
- * obtain a copy of the License at 
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
- * License for the specific language governing permissions and limitations under 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * User: Nityan Khanna
  * Date: 2019-1-31
  */
+
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Week5Attributes
 {
@@ -26,11 +25,11 @@ namespace Week5Attributes
 	/// Represents a custom attribute.
 	/// </summary>
 	/// <seealso cref="System.Attribute" />
-	// declare our custom attribute class, by inheriting from System.Attribute.	
+	// declare our custom attribute class, by inheriting from System.Attribute.
 	// we use the AttributeUsage class to specify where our attribute can be applied
 	// by using the AttributeTargets enum, we specify that
 	// our attribute can only be applied to classes
- 	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
 	public class MyCustomAttribute : Attribute
 	{
 		/// <summary>
@@ -38,7 +37,6 @@ namespace Week5Attributes
 		/// </summary>
 		public MyCustomAttribute()
 		{
-			
 		}
 
 		/// <summary>
